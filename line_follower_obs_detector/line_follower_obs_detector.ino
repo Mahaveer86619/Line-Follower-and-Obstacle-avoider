85af868ec8cc747049b12c031899925a7b2112e0
Mahaveer prasad panigrahi <psatyam86619@gmail.com>
2023-11-10 15:28:32 +0530

    First Commit

--- line_follower_obs_detector/line_follower_obs_detector.ino
+++ line_follower_obs_detector/line_follower_obs_detector.ino
@@ -1,203 +1,355 @@
+/*
+
+The Original line detector and Obstacle avoider, 2 in 1, bot with bluetooth wireless feature built into it. 
+
+There are many modes of the robot:
+1. line follower
+2. obstacle avoider
+3. line follower and obstacle avoider
+4. remote control through bluetooth (through app)
+5. voice control through bluetooth (through app)
+
+*/
+
 #include <Servo.h>
+
+int ir_r = 10;
+int ir_l = 11;
+int echo = 4;
+int trig = 2;
+int motorR1 = 7;
+int motorR2 =8;
+int enR = 6;
+int motorL1 = 13;
+int motorL2 = 12;
+int enL =5; 
 
 Servo servo;
 
-const int trigPin = 2;  
-const int echoPin = 4; 
+bool state = false;
+int mode = 0;
 
-const int enaB = 5;
-const int enaA = 6;
-const int motorB1 = 7;
-const int motorB2 = 8;
-const int motorA1 = 12;
-const int motorA2 = 13;
+float hitDist = 45;
 
-const int ir_L = 10;
-const int ir_R = 11;
-
-int BTdata;
-
-float duration, distance_L, distance_R, distance_F;  
-
-int space = 15;
-
+float duration, distance_L, distance_R, distance_F;
 
 void setup() {
+  Serial.begin(9600);
 
-  Serial.begin(9600); // start serial communication at 9600bps
+  pinMode(ir_r,INPUT);
+  pinMode(ir_l,INPUT);
 
-  pinMode(trigPin, OUTPUT); // ultra sonic triger (sender)
-	pinMode(echoPin, INPUT); // ultra sonic echo (reciever)
+  pinMode(echo,INPUT);
+  pinMode(trig,OUTPUT);
 
-  pinMode(enaA, OUTPUT); // to control the speed of motor A
-  pinMode(enaB, OUTPUT); // to control the speed of motor B
-  pinMode(motorA1, OUTPUT); // to control the motor A's direction
-  pinMode(motorA2, OUTPUT);
-  pinMode(motorB1, OUTPUT); // to control the motor B's direction
-  pinMode(motorB2, OUTPUT);
+  pinMode(motorR1,OUTPUT);
+  pinMode(motorR2,OUTPUT);
 
-  pinMode(ir_L, INPUT); // declare Left ir sensor as input
-  pinMode(ir_R, INPUT); // declare Right ir sensor as input
+  pinMode(motorL1,OUTPUT);
+  pinMode(motorL2,OUTPUT);
 
-  analogWrite(enaA, 200); // The Duty Cycle 0 to 255 Enable Pin A for Motor1 Speed 
-  analogWrite(enaB, 200); // The Duty Cycle 0 to 255 Enable Pin B for Motor2 Speed 
+  pinMode(enR,OUTPUT);
+  pinMode(enL,OUTPUT);
 
-  servo.attach(3); // servo motor control is in pin ~3
+  servo.attach(3);
 
+  analogWrite(enR, 250); // doesnot work in the ic
+  analogWrite(enL, 250); // doesnot work in the ic
 }
 
 void loop() {
 
-  distance_F = Ultrasonic_read();
-  Serial.print("D F=");Serial.println(distance_F);
- 
- 
-  //if Right Sensor and Left Sensor are at White color then it will call forword function
-  if((digitalRead(ir_R) == 0)&&(digitalRead(ir_L) == 0)){
-    if(distance_F > space){
-      forword();
-    } else {
-      Check_side();
-    }  
-  }  
+  while (Serial.available() > 0) {
+    int data = Serial.read();
+
+    if(data == '0'){
+      state = false; // running = false
+      mode = data;
+      stop();
+    }
+
+    if(data == '1' || data == '2' || data == '3'){
+      state = true; // running = true
+      mode = data;
+    }
+
+    if (data = 'F') {
+      forward();
+    } else if (data == 'R') {
+      steep_right();
+    } else if (data == 'L') {
+      steep_left();
+    } else if (data == 'B') {
+      back();
+    }
+
+  }
+
+  if(state == false && mode == '0') {
+    stop();
+  }
   
-  //if Right Sensor is Black and Left Sensor is White then it will call turn Right function
-  else if((digitalRead(ir_R) == 1)&&(digitalRead(ir_L) == 0)){
-    turnRight();
-  }  
-  
-  //if Right Sensor is White and Left Sensor is Black then it will call turn Left function
-  else if((digitalRead(ir_R) == 0)&&(digitalRead(ir_L) == 1)){
-    turnLeft();
+  // line detector
+  if(state == true && mode == '1') {
+      
+    lineFollower();
+
+  }
+
+  // obstacle avoider
+  if(state == true && mode == '2') {
+
+    obstacleAvoider();
+
+  }
+
+  // checking components
+  if(state == true && mode == '3') {
+    check_components();
+  }
+
+  // hybrid
+  if(state == true && mode == '4') {
+    hybrid();
+  }
+}
+
+void check_components() {
+
+  Serial.println("☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐");
+  stop();
+  delay(100);
+  for (int angle = 90; angle <= 180; angle += 5)  {
+   servoPulse(angle);  
+  }
+  delay(300);
+  distance_R = ultrasonic_read();
+  Serial.println();
+  Serial.print("Distance Right= ");
+  Serial.println(distance_R);
+  Serial.println();
+  delay(100);
+  for (int angle = 180; angle >= 0; angle -= 5) {
+   servoPulse(angle);  
+  }
+  delay(500);
+  distance_L = ultrasonic_read();
+  Serial.println();
+  Serial.print("Distance Left= ");
+  Serial.println(distance_L);
+  Serial.println();
+  delay(100);
+  for (int angle = 0; angle <= 90; angle += 5) {
+   servoPulse(angle);  
+  }
+  delay(300);
+  Serial.println("☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐");
+
+  forward();
+  delay(500);
+  steep_right();
+  delay(500);
+  steep_right();
+  delay(500);
+  back();
+  delay(500);
+
+}
+
+void hybrid() {
+  distance_F = ultrasonic_read();
+
+  if (distance_F <= hitDist) {
+    stop();
+  }
+
+  lineFollower();
+}
+
+void obstacleAvoider() {
+  distance_F = ultrasonic_read();
+
+  if (distance_F <= hitDist) {
+    stop();
+    check_side();
+  }
+  else {
+    forward();
+  }
+}
+
+void lineFollower() {
+    if(digitalRead(ir_r)==0 && digitalRead(ir_l)==0){
+      forward();
+    }
+
+    if(digitalRead(ir_r)==1 && digitalRead(ir_l)==0){
+      right();
+      delay(80);
+    }
+
+    if(digitalRead(ir_r)==0 && digitalRead(ir_l)==1){
+      left();
+      delay(80);
+    }
+    
+    if(digitalRead(ir_r)==1 && digitalRead(ir_l)==1){
+      stop();
+
+      delay(100);
+    }
+}
+
+// void check_side_wheels(float distance_F) {
+//   stop();
+//   delay(100);
+
+//   right();
+//   delay(300);
+//   stop();
+//   delay(300);
+//   distance_R = ultrasonic_read();
+//   Serial.print("Distance Right= ");
+//   Serial.println(distance_R);
+//   left();
+//   delay(600);
+//   stop();
+//   delay(300);
+//   distance_L = ultrasonic_read();
+//   Serial.print("Distance Left= ");
+//   Serial.println(distance_L);
+//   right();
+//   delay(300);
+//   stop();
+
+//   if(distance_R > distance_L) {
+//     steep_right();
+//     delay(300);
+//   } 
+//   if (distance_R < distance_L) {
+//     steep_left();
+//     delay(300);
+//   } 
+//   if ((distance_R <= hitDist && distance_L <= hitDist) && distance_F <= hitDist) {
+//     steep_right();
+//     delay(1000);
+//   }
+// }
+
+void check_side(){
+
+  Serial.println("☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐");
+  stop();
+  delay(100);
+  for (int angle = 90; angle <= 180; angle += 5)  {
+   servoPulse(angle);  
+  }
+  delay(300);
+  distance_R = ultrasonic_read();
+  Serial.println();
+  Serial.print("Distance Right= ");
+  Serial.println(distance_R);
+  Serial.println();
+  delay(100);
+  for (int angle = 180; angle >= 0; angle -= 5) {
+   servoPulse(angle);  
+  }
+  delay(500);
+  distance_L = ultrasonic_read();
+  Serial.println();
+  Serial.print("Distance Left= ");
+  Serial.println(distance_L);
+  Serial.println();
+  delay(100);
+  for (int angle = 0; angle <= 90; angle += 5) {
+   servoPulse(angle);  
+  }
+  delay(300);
+  Serial.println("☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐☐");
+
+  if(distance_R > distance_L) {
+    steep_right();
+    delay(350); // right motor is more powerfull fo less delay
+  } else if (distance_R < distance_L) {
+    steep_left();
+    delay(500);
+  } else if (distance_R <= hitDist && distance_L <= hitDist){
+    steep_left();
+    delay(1000);
   } 
-     
-  delay(10);
 
 }
 
 void servoPulse (int angle){
-  // int pwm = (angle*11) + 500;      // Convert angle to microseconds
-  // digitalWrite(pin, HIGH);
-  // delayMicroseconds(pwm);
-  // digitalWrite(pin, LOW);
   servo.write(angle); //rotate arm of servo by given angle
   delay(50); // Refresh cycle of servo
 }
 
-float Ultrasonic_read(){
-  digitalWrite(trigPin, LOW);  
+float ultrasonic_read(){
+  digitalWrite(trig, LOW);  
 	delayMicroseconds(2);  
-	digitalWrite(trigPin, HIGH);  
+	digitalWrite(trig, HIGH);  
 	delayMicroseconds(10);  
-	digitalWrite(trigPin, LOW); 
+	digitalWrite(trig, LOW); 
 
-  float duration = pulseIn(echoPin, HIGH); // recieve signal
-  float distance = (duration*.0343)/2; // convert into cm by myltiplying bt speed of sound and dividing by 2
+  float duration = pulseIn(echo, HIGH); // recieve signal
+  float distance = (duration*.0343)/2;  // convert into cm by myltiplying bt speed of sound and dividing by 2
 
   return distance;
 }
 
-void compareDistance() { // Turns around the obstacle
-  if(distance_L > distance_R){
-  turnLeft();
+void forward(){
+  digitalWrite(motorR1,HIGH);
+  digitalWrite(motorR2,LOW);
 
-  delay(500);
-
-  forword();
-
-  delay(600);
-
-  turnRight();
-
-  delay(500);
-
-  forword();
-
-  delay(600);
-
-  turnRight();
-
-  delay(400);
-  } else {
-  turnRight();
-
-  delay(500);
-
-  forword();
-
-  delay(600);
-
-  turnLeft();
-
-  delay(500);
-
-  forword();
-
-  delay(600);  
-
-  turnLeft();
-
-  delay(400);
-  }
+  digitalWrite(motorL1,HIGH);
+  digitalWrite(motorL2,LOW);
 }
 
-void Check_side(){
-  Stop();
-  delay(100);
-  for (int angle = 70; angle <= 140; angle += 5)  {
-   servoPulse(angle);  
-  }
-  delay(300);
-  distance_R = Ultrasonic_read();
-  Serial.print("Distance Right= ");
-  Serial.println(distance_R);
-  delay(100);
-  for (int angle = 140; angle >= 0; angle -= 5)  {
-   servoPulse(angle);  
-  }
-  delay(500);
-  distance_L = Ultrasonic_read();
-  Serial.print("Distance Left= ");
-  Serial.println(distance_L);
-  delay(100);
-  for (int angle = 0; angle <= 70; angle += 5)  {
-   servoPulse(angle);  
-  }
-  delay(300);
-  compareDistance();
+void right(){
+  digitalWrite(motorR1,LOW);
+  digitalWrite(motorR2,LOW);
+
+  digitalWrite(motorL1,HIGH);
+  digitalWrite(motorL2,LOW);
 }
 
-void forword(){  //forword
-  digitalWrite(motorA1, HIGH); //Left Motor forword Pin 
-  digitalWrite(motorA2, LOW); //Left Motor backword Pin 
-  digitalWrite(motorB1, HIGH); //Right Motor forword Pin 
-  digitalWrite(motorB2, LOW); //Right Motor backword Pin 
+void steep_right() {
+  digitalWrite(motorR1,LOW);
+  digitalWrite(motorR2,HIGH);
+
+  digitalWrite(motorL1,HIGH);
+  digitalWrite(motorL2,LOW);
 }
- 
-void backword(){ //backword
-  digitalWrite(motorA1, LOW); //Left Motor forword Pin 
-  digitalWrite(motorA2, HIGH); //Left Motor backword Pin 
-  digitalWrite(motorB1, LOW); //Right Motor forword Pin 
-  digitalWrite(motorB2, HIGH); //Right Motor backword Pin 
+
+void left(){
+  digitalWrite(motorR1,HIGH);
+  digitalWrite(motorR2,LOW);
+
+  digitalWrite(motorL1,LOW);
+  digitalWrite(motorL2,LOW);
 }
- 
-void turnRight(){ //turnRight
-  digitalWrite(motorA1, HIGH); //Left Motor forword Pin
-  digitalWrite(motorA2, LOW); //Left Motor backword Pin  
-  digitalWrite(motorB1, LOW); //Right Motor forword Pin 
-  digitalWrite(motorB2, HIGH); //Right Motor backword Pin 
+
+void steep_left() {
+  digitalWrite(motorR1,HIGH);
+  digitalWrite(motorR2,LOW);
+
+  digitalWrite(motorL1,LOW);
+  digitalWrite(motorL2,HIGH);
 }
- 
-void turnLeft(){ //turnLeft
-  digitalWrite(motorA1, LOW); //Left Motor forword Pin
-  digitalWrite(motorA2, HIGH); //Left Motor backword Pin  
-  digitalWrite(motorB1, HIGH); //Right Motor forword Pin 
-  digitalWrite(motorB2, LOW); //Right Motor backword Pin 
+
+void stop(){
+    digitalWrite(motorR1,LOW);
+    digitalWrite(motorR2,LOW);
+
+    digitalWrite(motorL1,LOW);
+    digitalWrite(motorL2,LOW);
 }
- 
-void Stop(){ //stop
-  digitalWrite(motorA1, LOW); //Left Motor forword Pin 
-  digitalWrite(motorA2, LOW); //Left Motor backword Pin 
-  digitalWrite(motorB1, LOW); //Right Motor forword Pin 
-  digitalWrite(motorB2, LOW); //Right Motor backword Pin 
+
+void back() {
+  digitalWrite(motorR1,LOW);
+  digitalWrite(motorR2,HIGH);
+
+  digitalWrite(motorL1,LOW);
+  digitalWrite(motorL2,HIGH);
 }
